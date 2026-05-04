93e2f028881d71fcbedad4881ab2d697e28f4d9e
TheDummi <raydecrom@hotmail.com>
2026-05-04 23:18:31 +0200

    Patch submit source code of app

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -69,11 +69,14 @@ export default async function RootLayout({
 			<head>
 				<meta
 					http-equiv='Content-Security-Policy'
-					content="default-src 'self' https://canary.xernerx.com https://app.xernerx.com; 
-                             img-src 'self' data: https://cdn.discordapp.com; 
-                             media-src 'self' blob: https://cdn.discordapp.com; 
-                             connect-src 'self' https://canary.xernerx.com https://cdn.discordapp.com;
-                             style-src 'self' 'unsafe-inline';"
+					content="
+    default-src 'self' https://canary.xernerx.com https://app.xernerx.com; 
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com; 
+    style-src 'self' 'unsafe-inline'; 
+    img-src 'self' data: https://cdn.discordapp.com; 
+    media-src 'self' blob: https://cdn.discordapp.com; 
+    connect-src 'self' https://canary.xernerx.com https://app.xernerx.com https://cdn.discordapp.com;
+  "
 				/>
 
 				<Script
