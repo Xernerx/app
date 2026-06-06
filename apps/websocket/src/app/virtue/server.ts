aec9c250bff7e4da90c7422175f61a76195d3136
TheDummi <raydecrom@hotmail.com>
2026-06-06 14:48:13 +0200

    Patch submit source code of app

--- apps/websocket/src/app/virtue/server.ts
+++ apps/websocket/src/app/virtue/server.ts
@@ -8,22 +8,66 @@ const models = {
 	members: () => mongoose.models.members,
 };
 
-const methods = {
-	get: (model: any, body: any) => model.findOne(body),
-	create: (model: any, body: any) => model.create(body),
-	update: (model: any, body: any) => model.findOneAndUpdate({ id: body.id }, { $set: body }, { after: true, runValidators: true }),
-	delete: (model: any, body: any) => model.findOneAndDelete(body),
-};
+function getFilter(action: string, body: any) {
+	switch (action) {
+		case 'members':
+			return {
+				id: body.id,
+				guild: body.guild,
+			};
+
+		case 'users':
+		case 'guilds':
+			return {
+				id: body.id,
+			};
+
+		default:
+			throw new Error(`Unsupported action: ${action}`);
+	}
+}
 
 export default async function Server(msg: any) {
 	const getModel = models[msg.action as keyof typeof models];
 
-	if (!getModel) throw new Error('Unknown action');
-
-	const method = methods[msg.method as keyof typeof methods];
-	if (!method) throw new Error('Unknown method');
+	if (!getModel) {
+		throw new Error('Unknown action');
+	}
 
 	const model = getModel();
 
-	return method(model, msg.body);
+	switch (msg.method) {
+		case 'get':
+			return model.findOne(msg.body);
+
+		case 'create':
+			try {
+				return await model.create(msg.body);
+			} catch (error: any) {
+				if (error?.code === 11000) {
+					return model.findOne(msg.body);
+				}
+
+				throw error;
+			}
+
+		case 'update':
+			return model.findOneAndUpdate(
+				getFilter(msg.action, msg.body),
+				{
+					$set: msg.body,
+				},
+				{
+					new: true,
+					runValidators: true,
+					upsert: false,
+				}
+			);
+
+		case 'delete':
+			return model.findOneAndDelete(getFilter(msg.action, msg.body));
+
+		default:
+			throw new Error('Unknown method');
+	}
 }
